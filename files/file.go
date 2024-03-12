7b6f0eb83e016f6ba9185b8ebd7576ae43ff56a3
sahil <sahil.dadia@hertz.net>
2024-03-12 16:58:59 +0000

    Added ls and ls recursive function

---
+++ files/file.go
@@ -0,0 +1,37 @@
+package files
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func ls(path string) ([]string, error) {
+	var fileNames = []string{}
+
+	dirEntries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range dirEntries {
+		fileNames = append(fileNames, entry.Name())
+	}
+
+	return fileNames, nil
+
+}
+
+func lsRecursive(path string) ([]string, error) {
+	var fileNames = []string{}
+	err := filepath.Walk(path,
+		func(path string, _ os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+	if err != nil {
+		return nil, err
+	}
+	return fileNames, nil
+}
